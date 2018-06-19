a3ac47faec46552b443c1d3a0217fc03e661c5be
qingyang <qing.muyi@foxmail.com>
2018-06-19 16:06:23 +0800

    add lucky draw test file

--- spring-boot-luckydraw/src/test/java/LuckyDrawTest.java
+++ spring-boot-luckydraw/src/test/java/LuckyDrawTest.java
@@ -78,7 +78,7 @@ public class LuckyDrawTest {
 
 
     public static void main(String[] args) throws InterruptedException {
-        final int N = 500;
+        final int N = 2000;
         Thread[] allSoldier = new Thread[N];
         boolean flag = false;
         CyclicBarrier cyclic = new CyclicBarrier(N, new BarrierRun(flag, N));
@@ -96,6 +96,8 @@ public class LuckyDrawTest {
             String resultJson = HttpUtil.createGet("http://127.0.0.1:8002/lucky/draw/query/"+v).execute().body();
             System.out.println("查询结果："+resultJson);
         });
+
+
     }
 
 }
