5c0a7ca0ea5e939920ee191fa82b3c33646ab1b6
qingyang <qing.muyi@foxmail.com>
2018-07-02 00:57:12 +0800

    -----

--- spring-boot-luckydraw/src/main/java/com/bugjc/ServiceApplication.java
+++ spring-boot-luckydraw/src/main/java/com/bugjc/ServiceApplication.java
@@ -12,6 +12,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 应用入口
@@ -26,6 +28,12 @@ public class ServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
+    }
+
+    @GetMapping(value = "/")
+    @ResponseBody
+    public String index() {
+        return "test";
     }
 
     @Override
