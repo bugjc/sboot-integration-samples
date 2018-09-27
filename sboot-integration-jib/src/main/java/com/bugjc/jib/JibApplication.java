764fbbc71cecfdd08ec6a0af506250ce08688771
qingyang <qing.muyi@foxmail.com>
2018-09-27 19:18:02 +0800

    日常更新

--- sboot-integration-jib/src/main/java/com/bugjc/jib/JibApplication.java
+++ sboot-integration-jib/src/main/java/com/bugjc/jib/JibApplication.java
@@ -1,5 +1,6 @@
 package com.bugjc.jib;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author qingyang
  * @date 2018/9/26 16:48
  */
+@Slf4j
 @SpringBootApplication
 public class JibApplication {
     public static void main(String[] args) {
@@ -20,6 +22,7 @@ public class JibApplication {
 
         @GetMapping("/")
         public String index(){
+            log.info("执行时间：{}",System.currentTimeMillis());
             return "index";
         }
     }
